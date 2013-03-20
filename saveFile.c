ec71e6c76005aacf25261b54e2a124bcb830fd2b
nielclement <niel.clement@gmail.com>
2013-03-20 12:02:49 +0100

    Stockage du contenu d'un fichier dans une variable (langage C)

---
+++ saveFile.c
@@ -0,0 +1,53 @@
+/*
+Programme qui stocke le contenu d'fichier dans une variable (ici tab).
+   ' '    qui reçoit un paramètre : le nom du fichier (avec le chemin).
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TAILLE_MAX 10000
+
+int main(int argc,char* argv[]) {
+	
+	char** tab=(char**)malloc((10000)*sizeof(char*));  //Contiendra l'ensemble des lignes.
+	char ** temp=tab;
+	char* parcours=(char*)malloc((TAILLE_MAX)*sizeof(char)); //Contiendra le contenu d'une ligne à la fois.
+	int j=0;
+  	FILE * fichier=fopen(argv[1],"r");
+  	
+  	if(argc!=2){
+  		printf("Nombre d'arguments incorrect.\n");
+  		exit(1);
+  	}
+  	
+  	if(fichier==NULL) {
+  		printf("Erreur lors du chargement du fichier.\n");
+  		exit(2);
+  	}
+
+  	while(fgets(parcours,TAILLE_MAX,fichier) != NULL) {
+      	temp[j]=(char*)malloc((strlen(parcours))*sizeof(char));
+      	strcpy(temp[j],parcours);
+      	j++;
+   	}
+  	
+  	/*Affichage du contenu du fichier:
+  	int tailleTab=0;
+  	tailleTab=j;
+  	printf("%d",tailleTab);
+  	j=0;
+ 		
+ 	while(j<tailleTab) {
+   		printf("%s",tab[j]);
+		j++;
+  	}
+  	*/
+  	
+  	free(tab);
+  	free(parcours);
+	fclose(fichier);
+	
+	return 0;
+}
